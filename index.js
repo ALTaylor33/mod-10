df5c15430a7de5a21420bf4370609cfd4d3a61e7
ALTaylor33 <austin.taylor33@rocketmail.com>
2023-04-04 21:38:26 -0400

    made prompts and started svg page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
+const {Circle, Triangle, Square} = require('./lib/shapes');
 
 const questions = [
   {
@@ -42,6 +43,25 @@ const questions = [
 ];
 
     inquirer.prompt(questions).then((answers) => {
-
+        console.log(answers)
+        let shapePage 
+        if (answers.shape === "square"){
+            shapePage = new Square()
+        }else if(answers.shape === "circle"){
+            shapePage = new Circle()
+        }else if(answers.shape === "triangle"){
+            shapePage = new Triangle()
+        }
+        console.log(shapePage)
+        shapePage.setColor(answers.color)
         
+
+
+
+
+
+//  fs.writeFile('mySvg.svg', svgString, (err) => {
+//       if (err) throw err;
+//       console.log('SVG image saved!');
+//     });
     })
